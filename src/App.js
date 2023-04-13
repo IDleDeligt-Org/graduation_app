a586e2786918e15b3c4f44d5c9cd4fe661bc7ee5
IdleDelight <123668111+IdleDelight@users.noreply.github.com>
2023-04-13 10:23:18 +0200

    Fix: Removed ref. to Search_Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import LoginPage from './Components/Login_page';
 import MainPage from './Components/Main_page';
-import SearchPage from './Components/Search_page';
 import DrinkPage from './Components/Drink_page';
 import IngredientsPage from './Components/Ingredients_page';
 import InspirationPage from './Components/Inspiration_page';

0ac9f8938ba16d60b432a5a894c1f8545b1c55b4
IdleDelight <123668111+IdleDelight@users.noreply.github.com>
2023-04-14 15:50:28 +0200

    SearchResults fix: drinks whout or broken images

--- src/Components/CocktailList.js
+++ src/Components/CocktailList.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import './CocktailList.css';
+import { MdBrokenImage } from 'react-icons/md';
 
-export default function CocktailList ({filteredCocktails, onCocktailSelect}) {
-  return (
-    <div className='cocktail-list'>
-      {filteredCocktails.map((cocktail, index) => {
-        return (
-          <div key={index} className='cocktail-item' onClick={() => onCocktailSelect(cocktail)}>
-            <div className='cocktail-name'>{cocktail.name}</div>
-            <img className='cocktail-image' src={cocktail.image} alt={cocktail.name} />
-          </div>
-        )
-      })}
-    </div>
-  )
+export default function CocktailList({ filteredCocktails, onCocktailSelect }) {
+    const handleImageLoad = (e) => {
+        e.target.style.display = 'block';
+        e.target.previousSibling.style.display = 'none';
+    };
+
+    const handleImageError = (e) => {
+        e.target.style.display = 'none';
+        e.target.previousSibling.style.display = 'flex';
+    };
+
+    return (
+        <div className="cocktail-list">
+            {filteredCocktails.map((cocktail, index) => {
+                return (
+                    <div key={index} className="cocktail-item" onClick={() => onCocktailSelect(cocktail)}>
+                        <div className="cocktail-image-placeholder">
+                            <MdBrokenImage className="broken-image-icon" />
+                        </div>
+                        <img
+                            className="cocktail-image"
+                            src={cocktail.image}
+                            alt={cocktail.name}
+                            onLoad={handleImageLoad}
+                            onError={handleImageError}
+                            style={{ display: 'none' }}
+                        />
+                        <div className="cocktail-name">{cocktail.name}</div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }

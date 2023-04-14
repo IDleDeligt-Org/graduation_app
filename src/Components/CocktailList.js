cdbb10fd112a24e0c55bcd373d47df61a4db0365
IdleDelight <123668111+IdleDelight@users.noreply.github.com>
2023-04-14 14:59:57 +0200

    DrinkPage layout done + searchResult done

--- src/Components/CocktailList.js
+++ src/Components/CocktailList.js
@@ -1,17 +1,17 @@
+import React from 'react';
+import './CocktailList.css';
+
 export default function CocktailList ({filteredCocktails, onCocktailSelect}) {
-
-    console.log(filteredCocktails)
-
-    return (
-        <div>
-            {filteredCocktails.map((cocktail, index) => {
-                return (
-                    <>
-                    <p key={index} onClick={() => onCocktailSelect(cocktail)}>{cocktail.name}</p>
-                    <img src="" alt=""></img>
-                    </>
-                )
-            })}
-        </div>
-    ) 
+  return (
+    <div className='cocktail-list'>
+      {filteredCocktails.map((cocktail, index) => {
+        return (
+          <div key={index} className='cocktail-item' onClick={() => onCocktailSelect(cocktail)}>
+            <div className='cocktail-name'>{cocktail.name}</div>
+            <img className='cocktail-image' src={cocktail.image} alt={cocktail.name} />
+          </div>
+        )
+      })}
+    </div>
+  )
 }

a16eb205a792b24b3dbf7d9c172f266d4323e93f
IdleDelight <123668111+IdleDelight@users.noreply.github.com>
2023-04-14 10:19:15 +0200

    Fixed nav blocking end content

--- src/Components/Nav_bar.js
+++ src/Components/Nav_bar.js
@@ -1,44 +1,38 @@
 import React from 'react';
 import './Nav_bar.css';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 
 const NavBar = ({ navigateTo, activePage }) => {
   return (
     <div className="NavBar">
-      <button onClick={() => navigateTo("ingredients")}>
-        <span
-          className={`material-icons ${activePage === "ingredients" ? "active" : ""}`}
-        >
-          liquor
-        </span>
+      <button
+        onClick={() => navigateTo("ingredients")}
+        className={activePage === "ingredients" ? "active" : ""}
+      >
+        <span className={`material-icons ${activePage === "ingredients" ? "active" : ""}`}>liquor</span>
       </button>
-      <button onClick={() => navigateTo("inspiration")}>
-        <span
-          className={`material-icons ${activePage === "inspiration" ? "active" : ""}`}
-        >
-          lightbulb
-        </span>
+      <button
+        onClick={() => navigateTo("inspiration")}
+        className={activePage === "inspiration" ? "active" : ""}
+      >
+        <span className={`material-icons ${activePage === "inspiration" ? "active" : ""}`}>lightbulb</span>
       </button>
-      <button onClick={() => navigateTo("main")}>
-        <span
-          className={`material-icons ${activePage === "main" ? "active" : ""}`}
-        >
-          local_bar
-        </span>
+      <button
+        onClick={() => navigateTo("main")}
+        className={activePage === "main" ? "active" : ""}
+      >
+        <span className={`material-icons ${activePage === "main" ? "active" : ""}`}>local_bar</span>
       </button>
-      <button onClick={() => navigateTo("favourites")}>
-        <span
-          className={`material-icons ${activePage === "favourites" ? "active" : ""}`}
-        >
-          favorite_border
-        </span>
+      <button
+        onClick={() => navigateTo("favourites")}
+        className={activePage === "favourites" ? "active" : ""}
+      >
+        <span className={`material-icons ${activePage === "favourites" ? "active" : ""}`}>favorite_border</span>
       </button>
-      <button onClick={() => navigateTo("settings")}>
-        <span
-          className={`material-icons ${activePage === "settings" ? "active" : ""}`}
-        >
-          settings
-        </span>
+      <button
+        onClick={() => navigateTo("settings")}
+        className={activePage === "settings" ? "active" : ""}
+      >
+        <span className={`material-icons ${activePage === "settings" ? "active" : ""}`}>settings</span>
       </button>
     </div>
   );

704ca9342fdbf47dac36b0ba8c6ea0cc2a19272d
KristianLF <123668111+IdleDelight@users.noreply.github.com>
2023-04-16 19:46:37 +0200

    Added and refined quickstart

--- src/Components/Main_Quickstart.js
+++ src/Components/Main_Quickstart.js
@@ -3,74 +3,74 @@ import './Main_Quickstart.css';
 import { MdBrokenImage } from 'react-icons/md';
 
 const MainQuickstart = ({ onQuickstartClick }) => {
-    const quickstartOptions = ['Gin', 'Rum', 'Tequila', 'Vodka', 'Whiskey', 'Non-alcoholic'];
+    const quickstartOptions = ['Gin', 'Rum', 'Tequila', 'Vodka', 'Whiskey', 'No alcohol'];
 
-  const [randomDrinks, setRandomDrinks] = useState([]);
+    const [randomDrinks, setRandomDrinks] = useState([]);
 
-  useEffect(() => {
-    fetchRandomDrinks();
-  }, []);
+    useEffect(() => {
+        fetchRandomDrinks();
+    }, []);
 
-  const fetchRandomDrinks = async () => {
-    try {
-      const drinks = [];
-      for (let i = 0; i < 4; i++) {
-        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
-        const data = await response.json();
-        drinks.push(data.drinks[0]);
-      }
-      setRandomDrinks(drinks);
-    } catch (error) {
-      console.error('Error fetching random drinks:', error);
-    }
-  };
+    const fetchRandomDrinks = async () => {
+        try {
+            const drinks = [];
+            for (let i = 0; i < 4; i++) {
+                const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+                const data = await response.json();
+                drinks.push(data.drinks[0]);
+            }
+            setRandomDrinks(drinks);
+        } catch (error) {
+            console.error('Error fetching random drinks:', error);
+        }
+    };
 
-  const handleImageLoad = (e) => {
-    e.target.style.display = 'block';
-    e.target.previousSibling.style.display = 'none';
-  };
+    const handleImageLoad = (e) => {
+        e.target.style.display = 'block';
+        e.target.previousSibling.style.display = 'none';
+    };
 
-  const handleImageError = (e) => {
-    e.target.style.display = 'none';
-    e.target.previousSibling.style.display = 'flex';
-  };
+    const handleImageError = (e) => {
+        e.target.style.display = 'none';
+        e.target.previousSibling.style.display = 'flex';
+    };
 
-  return (
-    <div className='main-quickstart'>
-      <div className='random-drinks-container'>
-        {randomDrinks.map((drink, index) => (
-          <div key={drink.idDrink} className="random-drink">
-            <div className="cocktail-item" onClick={() => onQuickstartClick(drink.strCategory)}>
-              <div className="cocktail-image-placeholder">
-                <MdBrokenImage className="broken-image-icon" />
-              </div>
-              <img
-                className="cocktail-image"
-                src={drink.strDrinkThumb}
-                alt={drink.strDrink}
-                onLoad={handleImageLoad}
-                onError={handleImageError}
-                style={{ display: 'none' }}
-              />
-              <div className="cocktail-name">{drink.strDrink}</div>
+    return (
+        <div className='main-quickstart'>
+            <div className='random-drinks-container'>
+                {randomDrinks.map((drink, index) => (
+                    <div key={drink.idDrink} className="random-drink">
+                        <div className="cocktail-item" onClick={() => onQuickstartClick(drink.strCategory)}>
+                            <div className="cocktail-image-placeholder">
+                                <MdBrokenImage className="broken-image-icon" />
+                            </div>
+                            <img
+                                className="cocktail-image"
+                                src={drink.strDrinkThumb}
+                                alt={drink.strDrink}
+                                onLoad={handleImageLoad}
+                                onError={handleImageError}
+                                style={{ display: 'none' }}
+                            />
+                            <div className="cocktail-name">{drink.strDrink}</div>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
 
-      <div className='quickstart-buttons'>
-        {quickstartOptions.map((option, index) => (
-          <button
-            key={index}
-            className='quickstart-button'
-            onClick={() => onQuickstartClick(option)}
-          >
-            {option}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+            <div className='quickstart-buttons'>
+                {quickstartOptions.map((option, index) => (
+                    <button
+                        key={index}
+                        className='quickstart-button'
+                        onClick={() => onQuickstartClick(option)}
+                    >
+                        <span className='quickstart-button-text'>{option}</span>
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default MainQuickstart;

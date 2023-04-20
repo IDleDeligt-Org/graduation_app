db781de56e4805355cf1322bf306ca825694c7a5
KristianLF <123668111+IdleDelight@users.noreply.github.com>
2023-04-20 23:14:50 +0200

    Favorites list done + some visual tweaks

--- src/Components/Favourites__page.js
+++ src/Components/Favourites__page.js
@@ -1,15 +1,25 @@
 import React, { useEffect } from "react";
+import { MdBrokenImage } from 'react-icons/md';
+import './Favourites_page.css';
 
 export default function FavouritePage({ favoriteList, setFavoriteList }) {
+  const handleImageLoad = (e) => {
+    e.target.style.display = 'block';
+    e.target.previousSibling.style.display = 'none';
+  };
 
+  const handleImageError = (e) => {
+    e.target.style.display = 'none';
+    e.target.previousSibling.style.display = 'flex';
+  };
   console.log("favoritePage rendered")
   const url = "https://sipster.azurewebsites.net/api/Favorite/user/"
 
   useEffect(() => {
     async function fetchFavorites() {
       await fetch(url + 2)
-      .then((response) => response.json())
-      .then((result) => setFavoriteList(result.$values))
+        .then((response) => response.json())
+        .then((result) => setFavoriteList(result.$values))
     }
     fetchFavorites();
   }, []);
@@ -20,12 +30,29 @@ export default function FavouritePage({ favoriteList, setFavoriteList }) {
 
     // Render the cocktails
     return flattenedBeverages.map((cocktail, index) => (
-      <div key={index} style={{color: "Black"}}>{cocktail.name}</div>
+      <div key={index} className="favorite-item">
+        <div className="favorite-image-placeholder">
+          <MdBrokenImage className="broken-image-icon" />
+        </div>
+        <img
+          className="favorite-image"
+          src={cocktail.image}
+          alt={cocktail.name}
+          onLoad={handleImageLoad}
+          onError={handleImageError}
+          style={{ display: 'none' }}
+        />
+        <div className="favorite-text">
+          <h2 className="favorite-name">{cocktail.name}</h2>
+          <p className="favotire-ingredients">{cocktail.beverageIngredients.$values.map((ingredients) => ingredients.ingredient.name).join(', ')}</p>
+        </div>
+
+      </div>
     ))
   }
 
   return (
-    <div>
+    <div className="favorite-container">
       {renderCocktails()}
     </div>
   );
